48dcced165a92f026ee5f5e3962d83c72a1ca0a7
Your Name <you@example.com>
2019-11-30 19:59:25 +0800

    creat a new Manage class

---
+++ Manage.java
@@ -0,0 +1,16 @@
+package com.google.chapter.wechat;
+
+import com.google.chapter.wechat.loginform.LoginFrom;
+
+/**
+ * @program: classprogram
+ * @Description: login
+ * @author: chun
+ * @date: 2019/11/27 下午
+ */
+public class Manage {
+    public static void main(String[] args) {
+       LoginFrom loginFrom = new LoginFrom();
+       loginFrom.setFrameLoginVisible(true);
+    }
+}
